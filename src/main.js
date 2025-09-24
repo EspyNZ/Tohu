b27d5e4edac8c2ddf21890fa612f003357bbf734
EspyNZ <samwapage@gmail.com>
2025-09-24 15:40:19 +1200

    Fix Google Maps timing issue by moving callback to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import './styles/main.css'
 import { TourGenerator } from './js/tour-generator.js'
 import { UIController } from './js/ui-controller.js'
+import { PlacesService } from './js/places-service.js'
 
 console.log('main.js: Script started.')
+
+// Define the Google Maps callback function globally
+window.initGoogleMaps = function() {
+  console.log('Google Maps API loaded successfully (initGoogleMaps callback).')
+  
+  // Directly notify PlacesService that Google Maps is loaded
+  PlacesService.notifyMapsLoaded()
+  console.log('PlacesService notified that Google Maps is loaded')
+}
 
 // Declare and instantiate uiController directly on the window object
 // This makes it globally accessible as soon as the script loads.
